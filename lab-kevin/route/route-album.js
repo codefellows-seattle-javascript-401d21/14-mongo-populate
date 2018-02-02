6492776fd7ddc72c9cdae804fbba9f6ef2362f09
Kevin Miller <kevin@driftabout.com>
2018-02-01 17:25:00 -0800

    created models

--- lab-kevin/route/route-album.js
+++ lab-kevin/route/route-album.js
@@ -1,1 +1,22 @@
 'use strict';
+
+'use strict';
+
+const bodyParser = require('body-parser').json();
+const Album = require('../model/album');
+const errorHandler = require('../lib/error-handler');
+const debug = require('debug');
+
+module.exports = function(router){
+
+  router.route('/:id?')
+    .get()
+    .post(bodyParser, (req, res) => {
+      Album(req.body).save()
+        .then(img => res.status(201).send(img))
+        .catch(err => errorHandler(err,res));
+    })
+    .put()
+    .delete();
+  
+};
--- lab-kevin/route/route-image.js
+++ lab-kevin/route/route-image.js
@@ -1,1 +1,20 @@
 'use strict';
+
+const bodyParser = require('body-parser').json();
+const Image = require('../model/image');
+const errorHandler = require('../lib/error-handler');
+const debug = require('debug');
+
+module.exports = function(router){
+
+  router.route('/:id?')
+    .get()
+    .post(bodyParser, (req, res) => {
+      Image(req.body).save()
+        .then(img => res.status(201).send(img))
+        .catch(err => errorHandler(err,res));
+    })
+    .put()
+    .delete();
+  
+};
