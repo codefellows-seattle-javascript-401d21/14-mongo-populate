4fb5a2fb679fd6c940f6b7bfd959fd9b3e796c59
Kevin Miller <kevin@driftabout.com>
2018-02-01 21:19:31 -0800

    Finished routes

--- lab-kevin/__test__/integration-image/image-delete.test.js
+++ lab-kevin/__test__/integration-image/image-delete.test.js
@@ -6,41 +6,72 @@ const superagent = require('superagent');
 require('jest');
 
 describe('DELETE Integration', function() {
-  beforeAll(() => server.start(process.env.PORT), () => console.log(process.env.PORT));
+  beforeAll(() => server.start());
   afterAll(() => server.stop());
   
   describe('Valid requests', () => {
 
+    this.newImage = {
+      file_path: '/Users/driftabout/Pictures/MISC_PIC/KMP_010115_4259.jpg',
+      photographer: 'Kevin Miller',
+      title: 'Robot',
+      description: 'Seriously, its not a chicken.' ,
+      location: 'Seattle, Wa',
+      album: '5a73c6b669a8cb69eaafbe10',
+    };
+
     beforeAll(()=> {
-      return  superagent.post(':4000/api/v1/note')
-        .send({subject: 'hello', comment: 'Funkn-A'})
+      return  superagent.post(':4000/api/v1/image')
+        .send(this.newImage)
         .then( res => {
           this.resPost = res;
+          return;
         })
         .catch(err => {
           debug('superagent error ', err);
         });
     });
 
-    describe('DELETE /api/v1/note/someid => whack', () => {
+    describe('DELETE /api/v1/image/someid ', () => {
       
       beforeAll(() => {
-        return superagent.delete(`:4000/api/v1/note/${this.resPost.body.id}`)
+        return superagent.delete(`:4000/api/v1/image/${this.resPost.body._id}`)
           .then(res => this.deleteRes = res);       
       });
       beforeAll(() => {
-        return superagent.get(`:4000/api/v1/note/${this.resPost.body.id}`)
-          .catch(err => this.deleteGet = err);       
+        return superagent.get(`:4000/api/v1/image/${this.resPost.body._id}`)
+          .catch(err => this.getErr = err);       
       });
 
       it('should return status 404', () => {
-        debug('this.deleteGet.body', this.deleteGet.status);
-        expect(this.deleteGet.status).toEqual(404);
+        debug('this.deleteGeterr.status', this.getErr.status);
+        expect(this.getErr.status).toEqual(404);
       });
       it('should return status code 204', () => {
         expect(this.deleteRes.status).toEqual(204);
       });
     });
   });
+
+  // describe('Invalid requests', () => {
+
+  //   beforeAll(() => {
+  //     return superagent.delete(`:4000/api/v1/${this.resPost.body._id}`)
+  //       .catch(err => this.deleteErr = err);       
+  //   });
+  //   beforeAll(() => {
+  //     return superagent.delete(':4000/api/v1/image/98457737d9934wy6a9w45q90c')
+  //       .catch(err => this.badDelete = err);       
+  //   });
+
+  //   it('should return status 404 when DELETE request to a bad path', () => {
+  //     expect(this.deleteErr.status).toEqual(404);
+  //   });
+
+  //   it('should return status 404 when DELETE request with a bad id', () => {
+  //     expect(this.badDelete.status).toEqual(400);
+  //   });
+
+  // });
 
 });
--- lab-kevin/__test__/integration-image/image-put.test.js
+++ lab-kevin/__test__/integration-image/image-put.test.js
@@ -6,13 +6,28 @@ const superagent = require('superagent');
 require('jest');
 
 describe('PUT Integration', function() {
-  beforeAll(() => server.start(process.env.PORT), () => console.log(process.env.PORT));
+  beforeAll(() => server.start());
   afterAll(() => server.stop());
   
   describe('Valid requests', () => {
+
+    this.newImage = {
+      file_path: '/Users/driftabout/Pictures/MISC_PIC/KMP_010115_4258.jpg',
+      photographer: 'Kevin Miller',
+      title: 'Door',
+      description: 'large round metal door handles on double wood door' ,
+      location: 'Seattle, Wa',
+      album: '5a73c6b669a8cb69eaafbe10',
+    };
+
+    this.updateImage = {
+      'title': 'Boat',
+      'description': 'old boat tied to a dock' ,
+    };
+
     beforeAll(()=> {
-      return  superagent.post(':4000/api/v1/note')
-        .send({subject: 'hello', comment: 'Funkn-A'})
+      return  superagent.post(':4000/api/v1/image')
+        .send(this.newImage)
         .then( res => {
           this.resPost = res;
         })
@@ -21,22 +36,26 @@ describe('PUT Integration', function() {
         });
     });
 
-    describe('PUT /api/v1/note/someid => update', () => {
-      this.mock_data = {subject: 'goodbye', comment: 'Funkn-B'};
+    describe('PUT /api/v1/image/someid => update', () => {
       beforeAll(() => {
-        return superagent.put(`:4000/api/v1/note/${this.resPost.body.id}`)
-          .send(this.mock_data)
+        return superagent.put(`:4000/api/v1/image/${this.resPost.body._id}`)
+          .send(this.updateImage)
           .then(res => this.put = res);       
       });
       beforeAll(() => {
-        return superagent.get(`:4000/api/v1/note/${this.resPost.body.id}`)
+        return superagent.get(`:4000/api/v1/image/${this.resPost.body._id}`)
           .then(res => this.putGet = res);       
       });
 
       it('should subject should be new', () => {
-        debug('this.putGet.body', this.putGet.body);
+        // debug('this.putGet.body', this.putGet.body);
         let body = JSON.parse(this.putGet.text);
-        expect(body.subject).toEqual(this.mock_data.subject);
+        expect(body.title).toEqual(this.updateImage.title);
+      });
+
+      it(' should be new', () => {
+        let body = JSON.parse(this.putGet.text);
+        expect(body.title).not.toEqual(this.putGet.title);
       });
       it('should return status code 204', () => {
         expect(this.put.status).toEqual(204);
@@ -46,4 +65,31 @@ describe('PUT Integration', function() {
   
   });
 
+
+
+  // describe('Invalid requests', () => {
+
+  //   describe('PUT /api/v1/image/someid => update', () => {
+  //     beforeAll(() => {
+  //       return superagent.put(':4000/api/v1/image/489736983F2939A938489E9W348C')
+  //         .send(this.updateImage)
+  //         .catch(err => this.putErr = err );    
+  //     });
+
+  //     it('should return status code 400 for an update with a bad id', () => {
+  //       expect(this.putErr.status).toEqual(400);
+  //     });
+
+  //     beforeAll(() => {
+  //       return superagent.put(`:4000/api/v1/${this.resPost.body._id}`)
+  //         //.send({subject: 'clouds'})
+  //         .send(this.newImage)
+  //         .catch(err => this.putEmpty = err );    
+  //     });
+
+  //     it('should return status code 5404 for bad path', () => {
+  //       expect(this.putEmpty.status).toEqual(404);
+  //     });
+  //   }); 
+  // });
 });
